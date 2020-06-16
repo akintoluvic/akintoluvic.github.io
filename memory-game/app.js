2ef0456ee7f27dea6018282a0d95a9f5e15d44bb
Vick Greenfields <akintoluvic@users.noreply.github.com>
2020-06-16 14:44:55 +0100

    yay! game works

--- memory-game/app.js
+++ memory-game/app.js
@@ -54,6 +54,10 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
     const grid = document.querySelector('.grid')
+    const displayResult = document.querySelector('#result')
+    var cardsChosen = []
+    var cardsChosenId = []
+    var cardsWon = []
 
     // Create game board
     function createBoard() {
@@ -61,8 +65,42 @@ document.addEventListener('DOMContentLoaded', () => {
             var card = document.createElement('img')
             card.setAttribute('src', 'images/color.png')
             card.setAttribute('data-id', i)
-            // card.addEventListener('click', flipCard)
+            card.addEventListener('click', flipCard)
             grid.appendChild(card)
+        }
+    }
+
+    // check for matches
+    function checkForMatch() {
+        var cards = document.querySelectorAll('img')
+        const optionOneId = cardsChosenId[0]
+        const optionTwoId = cardsChosenId[1]
+        if (cardsChosen[0] === cardsChosen[1]) {
+            alert('You found a match')
+            cards[optionOneId].setAttribute('src', 'images/blank.png')
+            cards[optionTwoId].setAttribute('src', 'images/blank.png')
+            cardsWon.push(cardsChosen)
+        } else {
+            cards[optionOneId].setAttribute('src', 'images/blank.png')
+            cards[optionTwoId].setAttribute('src', 'images/blank.png')
+            alert('Sorry, try again')
+        }
+        cardsChosen = []
+        cardsChosenId = []
+        displayResult.textContent = cardsWon.length
+        if (cardsWon.length === cardArray.length/2) {
+            displayResult.textContent = 'Congratulations! You found them all'
+        }
+    }
+
+    // flip your card
+    function flipCard() {
+        var cardId = this.getAttribute('data-id')
+        cardsChosen.push(cardArray[cardId].name)
+        cardsChosenId.push(cardId)
+        this.setAttribute('src', cardArray[cardId].img)
+        if (cardsChosen.length === 2) {
+            setTimeout(checkForMatch, 500)
         }
     }
 
