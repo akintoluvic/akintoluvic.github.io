b14240572c5f88d2a21684642efb7cb9a9888f10
Vick Greenfields <akintoluvic@users.noreply.github.com>
2020-06-16 14:13:52 +0100

    created crd array and board fn

---
+++ memory-game/app.js
@@ -0,0 +1,65 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const cardArray = [
+        {
+            name: 'bicycle',
+            img: 'images/bicycle.png'
+        },
+        {
+            name: 'camera',
+            img: 'images/camera.png'
+        },
+        {
+            name: 'owl',
+            img: 'images/owl.png'
+        },
+        {
+            name: 'radio',
+            img: 'images/radio.png'
+        },
+        {
+            name: 'rainbow',
+            img: 'images/rainbow.png'
+        },
+        {
+            name: 'cassette',
+            img: 'images/cassette.png'
+        },
+        {
+            name: 'bicycle',
+            img: 'images/bicycle.png'
+        },
+        {
+            name: 'camera',
+            img: 'images/camera.png'
+        },
+        {
+            name: 'owl',
+            img: 'images/owl.png'
+        },
+        {
+            name: 'radio',
+            img: 'images/radio.png'
+        },
+        {
+            name: 'rainbow',
+            img: 'images/rainbow.png'
+        },
+        {
+            name: 'cassette',
+            img: 'images/cassette.png'
+        },
+    ]
+})
+
+const grid = document.querySelector('.grid')
+
+// Create game board
+function createBoard() {
+    for (let i = 0; i < cardArray.length; i++) {
+        var card = document.createElement('img')
+        card.setAttribute('src', 'images/blank.png')
+        card.setAttribute('data-id', i)
+        // card.addEventListener('click', flipCard)
+        grid.appendChild(card)
+    }
+}
