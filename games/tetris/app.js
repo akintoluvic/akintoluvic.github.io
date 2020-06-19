e48163fcc60fb90e97eb5690836f4da761cc7e44
Vick Greenfields <akintoluvic@users.noreply.github.com>
2020-06-19 13:21:53 +0100

    issue no tetris showing

--- games/tetris/app.js
+++ games/tetris/app.js
@@ -6,112 +6,132 @@ document.addEventListener('DOMContentLoaded', () => {
     const height = 20
     let currentPosition = 4
 
+
+
+    //assign function to key codes
+    function control(e) {
+        if (e.keyCode === 39) {
+            moveRight()
+        } else if (e.keyCode === 38) {
+            rotate()
+        } else if (e.keyCode === 37) {
+            moveLeft()
+        } else if (e.keyCode === 40) {
+            moveDown()
+        }
+    }
+
+    document.addEventListener('keydown', control)
+
+
     //The Tetrominoes
-  const lTetromino = [
-    [1, width + 1, width * 2 + 1, 2],
-    [width, width + 1, width + 2, width * 2 + 2],
-    [1, width + 1, width * 2 + 1, width * 2],
-    [width, width * 2, width * 2 + 1, width * 2 + 2]
-  ]
+    const lTetromino = [
+        [1, width + 1, width * 2 + 1, 2],
+        [width, width + 1, width + 2, width * 2 + 2],
+        [1, width + 1, width * 2 + 1, width * 2],
+        [width, width * 2, width * 2 + 1, width * 2 + 2]
+    ]
 
-  const zTetromino = [
-    [0, width, width + 1, width * 2 + 1],
-    [width + 1, width + 2, width * 2, width * 2 + 1],
-    [0, width, width + 1, width * 2 + 1],
-    [width + 1, width + 2, width * 2, width * 2 + 1]
-  ]
+    const zTetromino = [
+        [0, width, width + 1, width * 2 + 1],
+        [width + 1, width + 2, width * 2, width * 2 + 1],
+        [0, width, width + 1, width * 2 + 1],
+        [width + 1, width + 2, width * 2, width * 2 + 1]
+    ]
 
-  const tTetromino = [
-    [1, width, width + 1, width + 2],
-    [1, width + 1, width + 2, width * 2 + 1],
-    [width, width + 1, width + 2, width * 2 + 1],
-    [1, width, width + 1, width * 2 + 1]
-  ]
+    const tTetromino = [
+        [1, width, width + 1, width + 2],
+        [1, width + 1, width + 2, width * 2 + 1],
+        [width, width + 1, width + 2, width * 2 + 1],
+        [1, width, width + 1, width * 2 + 1]
+    ]
 
-  const oTetromino = [
-    [0, 1, width, width + 1],
-    [0, 1, width, width + 1],
-    [0, 1, width, width + 1],
-    [0, 1, width, width + 1]
-  ]
+    const oTetromino = [
+        [0, 1, width, width + 1],
+        [0, 1, width, width + 1],
+        [0, 1, width, width + 1],
+        [0, 1, width, width + 1]
+    ]
 
-  const iTetromino = [
-    [1, width + 1, width * 2 + 1, width * 3 + 1],
-    [width, width + 1, width + 2, width + 3],
-    [1, width + 1, width * 2 + 1, width * 3 + 1],
-    [width, width + 1, width + 2, width + 3]
-  ]
+    const iTetromino = [
+        [1, width + 1, width * 2 + 1, width * 3 + 1],
+        [width, width + 1, width + 2, width + 3],
+        [1, width + 1, width * 2 + 1, width * 3 + 1],
+        [width, width + 1, width + 2, width + 3]
+    ]
 
-  const theTetrominoes = [lTetromino, zTetromino, tTetromino, oTetromino, iTetromino]
-
-
-  // Randomly select Tetromino
-  let random = Math.floor(Math.random() * theTetrominoes.length)
-  let currentRotation = 0
-  let currentTetromino = theTetrominoes[random][currentRotation]
+    const theTetrominoes = [lTetromino, zTetromino, tTetromino, oTetromino, iTetromino]
 
 
-  // draw tetromino shape
-  function drawTetromino() {
-      currentTetromino.forEach( index => {
-          squares[currentPosition + index].classList.add('block')
-      })
-  }
-
-  // undraw tetromino shape
-  function unDrawTetromino() {
-    currentTetromino.forEach((index) => {
-      squares[currentPosition + index].classList.remove("block");
-    });
-  }
-
-  // move tetromino down
-  function moveDown() {
-      unDrawTetromino()
-      currentPosition = currentPosition += width
-      drawTetromino()
-    //   freeze()
-  }
+    // Randomly select Tetromino
+    let random = Math.floor(Math.random() * theTetrominoes.length)
+    let currentRotation = 0
+    let currentTetromino = theTetrominoes[random][currentRotation]
 
 
-  // move right and prevent collision
-  function moveRight() {
-      unDrawTetromino()
-      const isAtRightEdge = currentTetromino.some(index => (currentPosition + index) % width === width - 1)
-      if (!isAtRightEdge) currentPosition += 1
-      if (currentTetromino.some(index => squares[currentPosition + index].classList.contains('block2'))) {
-          currentPosition -= 1
-      }
-      drawTetromino()
-  }
+    // draw tetromino shape
+    function drawTetromino() {
+        currentTetromino.forEach( index => {
+            squares[currentPosition + index].classList.add('block')
+        })
+    }
 
-  // move left and prevent collision
-  function moveLeft() {
-      unDrawTetromino()
-      const isAtLeftEdge = currentTetromino.some(index => (currentPosition + index) % width === 0)
-      if (!isAtLeftEdge) currentPosition -= 1
-      if (currentTetromino.some(index => squares[currentPosition + index].classList.contains('block2'))) {
-          currentPosition += 1
-      }
-      drawTetromino()
-  }
+    // undraw tetromino shape
+    function unDrawTetromino() {
+        currentTetromino.forEach((index) => {
+        squares[currentPosition + index].classList.remove("block");
+        });
+    }
 
-
-  // rotate Tetromino
-  function rotate() {
-      unDrawTetromino()
-      currentRotation++
-      if (currentRotation === currentTetromino.length) {
-          currentRotation = 0
-      }
-      currentTetromino = theTetrominoes[random][currentRotation]
-      drawTetromino()
-  }
+    // move tetromino down
+    function moveDown() {
+        unDrawTetromino()
+        console.log(currentPosition)
+        currentPosition = currentPosition += width
+        console.log(currentPosition)
+        drawTetromino()
+        //   freeze()
+    }
 
 
+    // move right and prevent collision
+    function moveRight() {
+        unDrawTetromino()
+        const isAtRightEdge = currentTetromino.some(index => (currentPosition + index) % width === width - 1)
+        if (!isAtRightEdge) currentPosition += 1
+        if (currentTetromino.some(index => squares[currentPosition + index].classList.contains('block2'))) {
+            currentPosition -= 1
+        }
+        drawTetromino()
+    }
+
+    // move left and prevent collision
+    function moveLeft() {
+        unDrawTetromino()
+        const isAtLeftEdge = currentTetromino.some(index => (currentPosition + index) % width === 0)
+        if (!isAtLeftEdge) currentPosition -= 1
+        if (currentTetromino.some(index => squares[currentPosition + index].classList.contains('block2'))) {
+            currentPosition += 1
+        }
+        drawTetromino()
+    }
+
+
+    // rotate Tetromino
+    function rotate() {
+        unDrawTetromino()
+        currentRotation++
+        if (currentRotation === currentTetromino.length) {
+            currentRotation = 0
+        }
+        currentTetromino = theTetrominoes[random][currentRotation]
+        drawTetromino()
+    }
 
 
 
+
+    drawTetromino()
 
 
 
